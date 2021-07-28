f5571092be9a80484965adfbccce09d1e4763eda
RajaniRanjan <rajani.ranjan@intel.com>
2021-07-28 13:59:25 +0530

    Add input support in Landscape from Rotate operation
    Tracked-On: OAM-97350
    Signed-off-by: RajaniRanjan <rajani.ranjan@intel.com>

--- client/include/vInputClient.h
+++ client/include/vInputClient.h
@@ -1,6 +1,11 @@
+#define XRES_MAX 540.00
+#define YRES_MAX 960.00
+#define LTRB_Y   327.00
+typedef enum {PRESS, RELEASE, MOVE} action;
+//typedef enum {ROTATION_0, ROTATION_1} rotation;
+
 int initvInputClient(const char []);
 void send_sync_event();
 void send_event(int32_t type, int32_t code, int32_t value);
-bool vinput_mouse_position(uint32_t x, uint32_t y);
-bool vinput_touch_release(u_int32_t x, u_int32_t y);
-bool vinput_touch_press(u_int32_t x, u_int32_t y);
+bool vinput_touch(action a, u_int32_t x, u_int32_t y, int rotation);
+
